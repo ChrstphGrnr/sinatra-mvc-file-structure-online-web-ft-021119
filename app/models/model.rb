83454afb0efc09a5ed442126cf78786349b33f8f
Christoph Gruener <christophgruener@me.com>
2019-04-01 16:21:14 +0000

    Automatically backed up by Learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,18 +1,4 @@
 class Model
 
-  attr_accessor :name, :breed, :age
 
-  @@all = []
-
-  def initialize(name= nil, breed= nil, age= nil)
-    @age = age
-    @name = name
-    @breed = breed
-    @@all << self
-  end
-
-  def self.all
-    @@all
-  end
-  
 end
